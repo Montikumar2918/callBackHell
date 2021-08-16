5013ae791711ab08384075b4c42299a19049214d
Monti kumar <kumarmonti445@gmail.com>
2021-08-16 14:14:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+value=document.getElementById("container")
+
+setTimeout(()=>{
+value.innerText="10";
+setTimeout(()=>{
+value.innerText="9";
+setTimeout(()=>{
+value.innerText="8";
+setTimeout(()=>{
+value.innerText="7";
+setTimeout(()=>{
+value.innerText="6";
+setTimeout(()=>{
+ value.innerText="5";
+ setTimeout(()=>{
+ value.innerText="4";
+setTimeout(()=>{
+value.innerText="3";
+setTimeout(()=>{
+ value.innerText="2";
+setTimeout(()=>{
+value.innerText="1";
+setTimeout(()=>{
+ value.innerText="HAPPY NEW YEAR!!!"
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
